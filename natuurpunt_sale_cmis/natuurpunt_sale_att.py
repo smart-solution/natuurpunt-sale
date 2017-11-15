47698ac96cf983458b5cd5551dbf232b7d6637f8
joeribelis <joeri.belis@natuurpunt.be>
2017-11-15 13:23:00 +0100

    [IMP] #4471 - cmis support

---
+++ natuurpunt_sale_cmis/natuurpunt_sale_att.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################## 
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class ir_attachment_type(osv.osv):
+    _name = 'ir.attachment.type'
+
+    _columns = {
+        'name': fields.char('Document type', size=128, required=True),
+    }
+
+ir_attachment_type()
+
+class sale_order_attachment(osv.osv):
+    _name = 'sale.order.attachment'
+
+    _columns = {
+        'order_id': fields.many2one('sale.order', 'Sale order', select=True),
+        'doc_type_id': fields.many2one('ir.attachment.type', 'Document type', select=True),
+        'name': fields.char('file name', help="sale order attachment"),
+    }
+
+sale_order_attachment()
+
+class sale_order(osv.osv):
+
+        _inherit = "sale.order"
+
+        _columns = {
+            'attachment_ids': fields.one2many('sale.order.attachment', 'order_id', 'sale order attachment'),
+        }
+
+class ir_attachment(osv.osv):
+
+    _inherit = 'ir.attachment'
+
+    def create(self, cr, uid, vals, context=None):
+        """Send the document to the CMIS server and create the attachment"""
+
+        res = super(ir_attachment, self).create(cr, uid, vals, context)
+        if vals['res_model'] == 'sale.order':
+            sale_order_att_obj = self.pool.get('sale.order.attachment')
+            att_vals = {
+                'order_id': vals['res_id'],
+                'name': vals['name']
+            }
+            sale_order_att_obj.create(cr,uid, att_vals, context)
+        return res
+
+    def unlink(self, cr, uid, ids, context=None):
+        """Delete the cmis document when a ressource is deleted"""
+        for att in self.browse(cr,uid, ids, context):
+            if att.res_model == 'sale.order':
+                sale_order_att_obj = self.pool.get('sale.order.attachment')
+                domain = [('order_id','=',att.res_id),('name','=',att.name)]
+                att_ids = sale_order_att_obj.search(cr,uid,domain)
+                sale_order_att_obj.unlink(cr,uid, att_ids, context=context)
+        res = super(ir_attachment, self).unlink(cr, uid, ids, context=context)
+        return res
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
