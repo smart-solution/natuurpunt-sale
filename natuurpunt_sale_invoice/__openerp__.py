338fd562745e3fb102c84e4c74fd967414a9f9f8
joeribelis <joeri.belis@natuurpunt.be>
2018-01-24 10:56:25 +0100

    [IMP] #4471 - origin button from invoice to sale order

---
+++ natuurpunt_sale_invoice/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################## 
+
+{
+    "name" : "Natuurpunt Sale Invoice",
+    "version" : "1.0",
+    "author" : "Joeri Belis",
+    "website" : "www.natuurpunt.be",
+    "category" : "sale",
+    "description": """
+    Natuurpunt Sale Invoice
+""",
+    "depends" : ["natuurpunt_account","natuurpunt_sale",],
+    "data" : ["natuurpunt_sale_invoice_view.xml",],
+    "init_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
---
+++ natuurpunt_sale_invoice/natuurpunt_sale_invoice.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class account_invoice(osv.osv):
+
+    _inherit = 'account.invoice'
+
+    def view_origin_so(self, cr, uid, ids, context=None):
+        view_id = self.pool.get('ir.ui.view').search(cr, uid, [('model','=','sale.order'),
+                                                            ('name','=','sale.order.form')])
+
+        sale_invoice_ids = self.pool.get('sale.invoice').search(cr,uid,[('invoice_id','in',ids)])
+        sale_invoice = self.pool.get('sale.invoice').browse(cr,uid,sale_invoice_ids)[0]
+
+# fill needed fields in context
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Brondocument',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'view_id': False,  #view_id[0],
+            'res_model': 'sale.order',
+            'target': 'current',
+            'context': context,
+            'res_id': sale_invoice.order_id.id,
+            }
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
